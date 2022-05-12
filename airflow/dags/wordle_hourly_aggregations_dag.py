b2b5ea4ba23f2b236efb2e3fbc4a9b77cad30137
Tomaz Cufer <zamot101@gmail.com>
2022-05-12 10:36:28 +0200

    Airflow rename dag and tasks.

--- airflow/dags/wordle_hourly_aggregations_dag.py
+++ airflow/dags/wordle_hourly_aggregations_dag.py
@@ -2,11 +2,11 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.postgres_operator import PostgresOperator
 
-POSTGRES_CONN_ID = "pg_dev"
+POSTGRES_CONN_ID = "wordle_pulse_db"
 TMPL_SEARCH_PATH = "dags/sql/"
 
 dag = DAG(
-    "wordle_hourly_agg_dag_2",
+    "wordle_hourly_aggregations_dag",
     template_searchpath=[TMPL_SEARCH_PATH],
     schedule_interval="0 * * * *",
     start_date=datetime(2022, 4, 6),
@@ -19,18 +19,18 @@ add_hourly_aggregations = PostgresOperator(
     sql="wordle_results_hourly.sql"
 )
 
-add_hourly_aggregations_top_results = PostgresOperator(
-    task_id="add_hourly_aggregations_top_results",
+add_hourly_aggregations_most_common_results = PostgresOperator(
+    task_id="add_hourly_aggregations_most_common_results",
     dag=dag,
     postgres_conn_id=POSTGRES_CONN_ID,
     sql="wordle_most_common_results_hourly.sql"
 )
 
-add_hourly_distribution_results = PostgresOperator(
-    task_id="add_hourly_distribution_results",
+add_hourly_results_distribution = PostgresOperator(
+    task_id="add_hourly_results_distribution",
     dag=dag,
     postgres_conn_id=POSTGRES_CONN_ID,
     sql="wordle_results_distribution_hourly.sql"
 )
 
-add_hourly_aggregations >> add_hourly_aggregations_top_results >> add_hourly_distribution_results
+add_hourly_aggregations >> add_hourly_aggregations_most_common_results >> add_hourly_results_distribution
